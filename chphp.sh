9681c301b1d44599ac8f4bed59943bf3a61187cd
Jonatan Frank <jonafrank@gmail.com>
2020-03-23 20:18:47 -0300

    add script

---
+++ chphp.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+if [ $1 = "--list" ] || [ $1 = "-l" ]; then
+    ls /usr/bin/php*
+    exit 0
+fi
+
+if [ $1 = "-h" ] || [ $1 = "--help"]; then
+    echo "chphp -h|--help: Display this help."
+    echo "chphp -l|--list: Display installed php versions."
+    echo "chphp php*.*: Change tho the given php version."
+    exit 0
+fi
+
+update-alternatives --set php /usr/bin/$1
+exit 0
