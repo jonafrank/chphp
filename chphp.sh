4f244f9f3532dec5fc73e9566bbbf8ab92a42d05
Jonatan Frank <jonafrank@gmail.com>
2020-03-23 20:23:00 -0300

    fix sapcing in if condition

--- chphp.sh
+++ chphp.sh
@@ -5,7 +5,7 @@ if [ $1 = "--list" ] || [ $1 = "-l" ]; then
     exit 0
 fi
 
-if [ $1 = "-h" ] || [ $1 = "--help"]; then
+if [ $1 = "-h" ] || [ $1 = "--help" ]; then
     echo "chphp -h|--help: Display this help."
     echo "chphp -l|--list: Display installed php versions."
     echo "chphp php*.*: Change tho the given php version."
